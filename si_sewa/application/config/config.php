e724d2e12fe7089b79b403c88f106e1292e0b335
harismansihombing <sihombingharisman28@gmail.com>
2020-07-02 11:26:18 +0700

    test
    test

--- si_sewa/application/config/config.php
+++ si_sewa/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/si_sewa/';
+$config['base_url'] = 'http://localhost/Project_Smt4_4C/si_sewa/';
 
 /*
 |--------------------------------------------------------------------------
