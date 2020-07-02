2481db8582951a81e1b9f76f51c0326ad55ad02b
harismansihombing <sihombingharisman28@gmail.com>
2020-07-02 11:31:06 +0700

    Update index.php
    Update dashboard penyewa

--- si_sewa/application/views/penyewa/index.php
+++ si_sewa/application/views/penyewa/index.php
@@ -24,10 +24,7 @@
                         <div class="col-sm-6">
                             <h4 class="m-0 text-dark">Dashboard</h4>
                         </div><!-- /.col -->
-                            
-                            <div>
-                                <a href="<?= base_url('penyewa/upload/'. $this->uri->segment(3)); ?>">Halaman Upload</a>
-                            </div>
+
 
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
@@ -38,7 +35,7 @@
                     </div><!-- /.row -->
                 </div><!-- /.container-fluid -->
             </div>
-            
+
             <section class="content">
                 <div class="container-fluid">
                     <!-- Info boxes -->
@@ -48,11 +45,12 @@
                                 <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
 
                                 <div class="info-box-content">
-                                    <span class="info-box-text">CPU Traffic</span>
+                                    <span class="info-box-text">Lapak Saya</span>
                                     <span class="info-box-number">
-                                        10
-                                        <small>%</small>
+                                        <br>
                                     </span>
+                                    <a href="#" class="small-box-footer">More info <i
+                                            class="fas fa-arrow-circle-right"></i></a>
                                 </div>
                                 <!-- /.info-box-content -->
                             </div>
@@ -61,7 +59,8 @@
                         <!-- /.col -->
                         <div class="col-12 col-sm-6 col-md-3">
                             <div class="info-box mb-3">
-                                <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+                                <span class="info-box-icon bg-danger elevation-1"><i
+                                        class="fas fa-thumbs-up"></i></span>
 
                                 <div class="info-box-content">
                                     <span class="info-box-text">Likes</span>
@@ -78,11 +77,14 @@
 
                         <div class="col-12 col-sm-6 col-md-3">
                             <div class="info-box mb-3">
-                                <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+                                <span class="info-box-icon bg-success elevation-1"><i
+                                        class="fas fa-shopping-cart"></i></span>
 
                                 <div class="info-box-content">
-                                    <span class="info-box-text">Sales</span>
-                                    <span class="info-box-number">760</span>
+                                    <span class="info-box-text">Transaksi Sewa</span>
+                                    <span class="info-box-number">12</span>
+                                    <a href="#" class="small-box-footer">More info <i
+                                            class="fas fa-arrow-circle-right"></i></a>
                                 </div>
                                 <!-- /.info-box-content -->
                             </div>
@@ -94,8 +96,10 @@
                                 <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
 
                                 <div class="info-box-content">
-                                    <span class="info-box-text">New Members</span>
-                                    <span class="info-box-number">2,000</span>
+                                    <span class="info-box-text">Riwayat Sewa</span>
+                                    <span class="info-box-number">20</span>
+                                    <a href="" class="small-box-footer">More info <i
+                                            class="fas fa-arrow-circle-right"></i></a>
                                 </div>
                                 <!-- /.info-box-content -->
                             </div>
@@ -105,141 +109,24 @@
                     </div>
                     <!-- /.row -->
 
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h5 class="card-title">Monthly Recap Report</h5>
 
-                                    <div class="card-tools">
-                                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                                            <i class="fas fa-minus"></i>
-                                        </button>
-                                        <div class="btn-group">
-                                            <button type="button" class="btn btn-tool dropdown-toggle" data-toggle="dropdown">
-                                                <i class="fas fa-wrench"></i>
-                                            </button>
-                                            <div class="dropdown-menu dropdown-menu-right" role="menu">
-                                                <a href="#" class="dropdown-item">Action</a>
-                                                <a href="#" class="dropdown-item">Another action</a>
-                                                <a href="#" class="dropdown-item">Something else here</a>
-                                                <a class="dropdown-divider"></a>
-                                                <a href="#" class="dropdown-item">Separated link</a>
-                                            </div>
-                                        </div>
-                                        <button type="button" class="btn btn-tool" data-card-widget="remove">
-                                            <i class="fas fa-times"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                                <!-- /.card-header -->
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-md-8">
-                                            <p class="text-center">
-                                                <strong>Sales: 1 Jan, 2014 - 30 Jul, 2014</strong>
-                                            </p>
 
-                                            <div class="chart">
-                                                <!-- Sales Chart Canvas -->
-                                                <canvas id="salesChart" height="180" style="height: 180px;"></canvas>
-                                            </div>
-                                            <!-- /.chart-responsive -->
-                                        </div>
-                                        <!-- /.col -->
-                                        <div class="col-md-4">
-                                            <p class="text-center">
-                                                <strong>Goal Completion</strong>
-                                            </p>
 
-                                            <div class="progress-group">
-                                                Add Products to Cart
-                                                <span class="float-right"><b>160</b>/200</span>
-                                                <div class="progress progress-sm">
-                                                    <div class="progress-bar bg-primary" style="width: 80%"></div>
-                                                </div>
-                                            </div>
-                                            <!-- /.progress-group -->
-
-                                            <div class="progress-group">
-                                                Complete Purchase
-                                                <span class="float-right"><b>310</b>/400</span>
-                                                <div class="progress progress-sm">
-                                                    <div class="progress-bar bg-danger" style="width: 75%"></div>
-                                                </div>
-                                            </div>
-
-                                            <!-- /.progress-group -->
-                                            <div class="progress-group">
-                                                <span class="progress-text">Visit Premium Page</span>
-                                                <span class="float-right"><b>480</b>/800</span>
-                                                <div class="progress progress-sm">
-                                                    <div class="progress-bar bg-success" style="width: 60%"></div>
-                                                </div>
-                                            </div>
-
-                                            <!-- /.progress-group -->
-                                            <div class="progress-group">
-                                                Send Inquiries
-                                                <span class="float-right"><b>250</b>/500</span>
-                                                <div class="progress progress-sm">
-                                                    <div class="progress-bar bg-warning" style="width: 50%"></div>
-                                                </div>
-                                            </div>
-                                            <!-- /.progress-group -->
-                                        </div>
-                                        <!-- /.col -->
-                                    </div>
-                                    <!-- /.row -->
-                                </div>
-                                <!-- ./card-body -->
-                                <div class="card-footer">
-                                    <div class="row">
-                                        <div class="col-sm-3 col-6">
-                                            <div class="description-block border-right">
-                                                <span class="description-percentage text-success"><i class="fas fa-caret-up"></i> 17%</span>
-                                                <h5 class="description-header">$35,210.43</h5>
-                                                <span class="description-text">TOTAL REVENUE</span>
-                                            </div>
-                                            <!-- /.description-block -->
-                                        </div>
-                                        <!-- /.col -->
-                                        <div class="col-sm-3 col-6">
-                                            <div class="description-block border-right">
-                                                <span class="description-percentage text-warning"><i class="fas fa-caret-left"></i> 0%</span>
-                                                <h5 class="description-header">$10,390.90</h5>
-                                                <span class="description-text">TOTAL COST</span>
-                                            </div>
-                                            <!-- /.description-block -->
-                                        </div>
-                                        <!-- /.col -->
-                                        <div class="col-sm-3 col-6">
-                                            <div class="description-block border-right">
-                                                <span class="description-percentage text-success"><i class="fas fa-caret-up"></i> 20%</span>
-                                                <h5 class="description-header">$24,813.53</h5>
-                                                <span class="description-text">TOTAL PROFIT</span>
-                                            </div>
-                                            <!-- /.description-block -->
-                                        </div>
-                                        <!-- /.col -->
-                                        <div class="col-sm-3 col-6">
-                                            <div class="description-block">
-                                                <span class="description-percentage text-danger"><i class="fas fa-caret-down"></i> 18%</span>
-                                                <h5 class="description-header">1200</h5>
-                                                <span class="description-text">GOAL COMPLETIONS</span>
-                                            </div>
-                                            <!-- /.description-block -->
-                                        </div>
-                                    </div>
-                                    <!-- /.row -->
-                                </div>
-                                <!-- /.card-footer -->
+                    <div class="col-md-9">
+                        <div class="card card-primary">
+                            <div class="card-body p-0">
+                                <!-- THE CALENDAR -->
+                                <div id="calendar"></div>
                             </div>
-                            <!-- /.card -->
+                            <!-- /.card-body -->
                         </div>
-                        <!-- /.col -->
+                        <!-- /.card -->
                     </div>
-                    
+
+
+
+
+
                 </div>
                 <!--/. container-fluid -->
             </section>
